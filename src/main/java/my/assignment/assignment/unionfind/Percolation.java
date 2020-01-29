e60690a33d56f133e25f4593ed7583eda4ec978e
zhaomin6666 <804122845@qq.com>
2020-01-29 23:37:57 +0800

    修改类路径

--- src/main/java/my/assignment/assignment/unionfind/Percolation.java
+++ src/main/java/my/assignment/assignment/unionfind/Percolation.java
@@ -1,4 +1,4 @@
-package my.assignment.assignment1;
+package my.assignment.assignment.unionfind;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
--- src/main/java/my/assignment/assignment/unionfind/PercolationStats.java
+++ src/main/java/my/assignment/assignment/unionfind/PercolationStats.java
@@ -1,4 +1,4 @@
-package my.assignment.assignment1;
+package my.assignment.assignment.unionfind;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
